e237a3dcc1d8b8adfb117302a67706021b58e1b9
myungjin <leemjin0607@gmail.com>
2022-07-27 00:52:59 +0900

    cat image

---
+++ Java_beakjoon/src/inputOutput_calculation/Exam03.java
@@ -0,0 +1,10 @@
+package inputOutput_calculation;
+
+public class Exam03 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
