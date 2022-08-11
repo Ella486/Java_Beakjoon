05c620b55b0e6b4f0c8a4c8bcae928182cb8e2d8
myungjin <leemjin0607@gmail.com>
2022-08-11 22:47:24 +0900

    입출력 응용문제 ??!

---
+++ Java_beakjoon/src/inputOutput_calculation/Exam10.java
@@ -0,0 +1,13 @@
+package inputOutput_calculation;
+
+import java.util.Scanner;
+
+public class Exam10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id + "??!");
+		
+		sc.close();
+	}
+}
