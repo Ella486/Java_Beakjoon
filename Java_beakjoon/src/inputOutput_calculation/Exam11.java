5168906b9f04a5403c26227c87a7bd16e93b84b7
myungjin <leemjin0607@gmail.com>
2022-08-11 22:58:11 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ Java_beakjoon/src/inputOutput_calculation/Exam11.java
@@ -0,0 +1,13 @@
+package inputOutput_calculation;
+
+import java.util.Scanner;
+
+public class Exam11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		System.out.println(y - 543);
+		
+		sc.close();
+	}
+}
